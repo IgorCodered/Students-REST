db2e6769a357fb1dcc74e8fe9777d8380c78a0bb
Igor Elesin <collidefor@outlook.com>
2023-11-22 12:00:37 +0700

    Start project

---
+++ src/main/java/ru/studentproject/crud/springstudents/SpringStudentsApp.java
@@ -0,0 +1,13 @@
+package ru.studentproject.crud.springstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentsApp.class, args);
+	}
+
+}
---
+++ src/test/java/ru/studentproject/crud/springstudents/SpringStudentsAppTests.java
@@ -0,0 +1,13 @@
+package ru.studentproject.crud.springstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentsAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
